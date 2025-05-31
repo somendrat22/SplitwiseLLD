72382ba1e3f9584a8889313530eda65f314e991f
Bucket123 <shubhujais7257@gmail.com>
2025-05-31 16:11:58 +0530

    Two Sum added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import controller.SplitWiseController;
 
 public class Main {
+    public int TwoSum(int a, int b)
+    {
+        return a+b;
+    }
     public static void main(String[] args) {
         SplitWiseController splitWiseController = new SplitWiseController();
         splitWiseController.startProgram();
