bcb5b3b3ad7f06ca2610be61051c46e60a51f9cc
Ashutosh Kumar Tiwari <Ashutoshkumartiwari50@gmail.com>
2025-05-31 18:58:58 +0530

    MyNew update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import controller.SplitWiseController;
 
 public class Main {
+    public int Sum(int a, int b){
+        return a+b;
+    }
     public static void main(String[] args) {
         SplitWiseController splitWiseController = new SplitWiseController();
         splitWiseController.startProgram();
