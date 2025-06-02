90d4ada78d07e6a9c3d69d9bd02272407208fee4
sachin <sachinguy88@gmail.com>
2025-06-02 10:47:45 +0530

    code update

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         SplitWiseController splitWiseController = new SplitWiseController();
         splitWiseController.startProgram();
+        System.out.println("Hello riya" + " ");
     }
 }
