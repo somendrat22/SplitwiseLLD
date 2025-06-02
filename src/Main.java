0e3fb6a2e618e99cc85fe237441c96aa7ca5e843
sachin <sachinguy88@gmail.com>
2025-06-02 17:14:05 +0530

    Exception handling done

--- src/Main.java
+++ src/Main.java
@@ -2,44 +2,34 @@ import controller.SplitWiseController;
 
 public class Main {
 
-
-
     public static void fun2() throws ClassNotFoundException{
-        throw new ClassNotFoundException("dnndjendjejdbejdb");
+        throw new ClassNotFoundException("sdf");
     }
 
-    public static void fun1() throws ClassNotFoundException{
+    public static void fun1 () throws ClassNotFoundException{
         fun2();
     }
+    public static void main(String[] args) {
+        /*int [] arr={1,4,7};
+        try {
+            System.out.println("Inside the catch");
+            int x = arr[3];
+            System.out.println("Behind the catch");
+        }catch(Exception e){
+            System.out.println("Exception catch");
+        }*/
 
 
-    public static void main(String[] args) throws ClassNotFoundException {
-//        SplitWiseController splitWiseController = new SplitWiseController();
-//        splitWiseController.startProgram();
-//        int a = 10;
-//        int b = 0;
+//        try {
+//            throw new ClassNotFoundException("nice");
+//        }catch(Exception w){
 //
-//        try{
-//            System.out.println("hey inside try");
-////            int [] arr = {10, 2, 4};
-////            int x = arr[10];
-//            Integer y = null;
-//            y.toString();
-//            System.out.println("behind try");
-//        }catch (ArrayIndexOutOfBoundsException e){
-//            System.out.println("Exception caught array ");
-//        }catch (ArithmeticException e){
-//            System.out.println("Exception Caught");
 //        }
-
-//       throw new NullPointerException("jdjiejd"); // unchecked exception
-
-           // throw new ClassNotFoundException("fndhccnjdfnjnfjndf");
-        try{
-
-        }
+        try {
             fun1();
-
+        }catch(Exception e){
+            System.out.println("hello");
+        }
     }
 
 }
